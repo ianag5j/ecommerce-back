2e5fd1a0a3a517cbdbbb2399266499f5b2220424
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-12-19 15:53:45 -0300

    feat: set vars to deploy

--- authorizer.tf
+++ authorizer.tf
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "authorizer" {
   role = aws_iam_role.lambda_exec.arn
   environment {
     variables = {
-      AUTH0_DOMAIN   = "https://dev-7yp156lleklcumhn.us.auth0.com/"
+      AUTH0_DOMAIN = var.auth0_domain
     }
   }
 }
@@ -28,11 +28,11 @@ resource "aws_cloudwatch_log_group" "authorizer" {
 # ############ API GATEWAY ############
 
 resource "aws_apigatewayv2_authorizer" "customAuthorizer" {
-  api_id           = aws_apigatewayv2_api.lambda.id
-  authorizer_payload_format_version= "2.0"
-  authorizer_uri= aws_lambda_function.authorizer.invoke_arn
-  authorizer_type  = "REQUEST"
-  identity_sources = ["$request.header.Authorization"]
-  name             = "ecommerce-custom-authorizer"
-  enable_simple_responses = true
+  api_id                            = aws_apigatewayv2_api.lambda.id
+  authorizer_payload_format_version = "2.0"
+  authorizer_uri                    = aws_lambda_function.authorizer.invoke_arn
+  authorizer_type                   = "REQUEST"
+  identity_sources                  = ["$request.header.Authorization"]
+  name                              = "ecommerce-custom-authorizer"
+  enable_simple_responses           = true
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "auth0_domain" {
+  description = "auth0 domain url."
+
+  type = string
+}
